a1a78077d317a0d1919478ed59717a03fb3a1d38
John Martens <john.martens@gmail.com>
2020-04-06 15:18:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame as game
+import random as r
+
+game.init()
+game.display.set_caption("Window Title Goes Here")
+screen = game.display.set_mode([800, 600])
+gameon = True
+bgcolor = game.color.Color("#f6cb39")
+alien = game.image.load("http://icons.iconarchive.com/icons/goodstuff-no-nonsense/free-space/256/space-invader-icon.png")
+# main loop of the game
+while gameon:
+    screen.fill(bgcolor)
+    screen.blit(alien, (100, 100))
+    game.display.flip()  # redraws / refreshes screen
+    event = game.event.poll()  # listens for mouse, keyboard, quitting
+    if event.type == game.QUIT:
+        gameon = False
+# comes here when game is over
+while True:
+    screen.fill(bgcolor)
+    game.display.flip()
+    event = game.event.poll()
+    if event.type == game.QUIT:
+        break
+game.quit()
